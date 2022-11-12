c4c18bafaa59b388e3f1c98c3b002e7e2cee07e4
SmaLLAlien <vlad_belousov1990@hotmail.com>
2022-11-12 20:43:50 +0200

    stacks: added stack implementation

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -1,21 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, Validators} from "@angular/forms";
+import {StackService} from "../stack.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'data_structures';
   taskControl = new FormControl('', Validators.required);
   array = [];
 
-  addItemToList(): void {
+  constructor(public stack: StackService<string>) {}
 
+
+  addItemToStack(): void {
+    this.stack.push(this.taskControl.value as string);
   }
 
-  deleteItem(i: number): void {
+  deleteItem(): void {
+    this.stack.pop();
+  }
+
+  ngOnInit(): void {
+    this.stack.push("David");
+    this.stack.push("Raymond");
+    this.stack.push("Bryan");
+
+    console.log("length: " + this.stack.length());
+    console.log(this.stack.peek());
+
+    const popped = this.stack.pop();
+    console.log("The popped element is: " + popped);
+    console.log(this.stack.peek());
+
+    this.stack.push("Cynthia");
+    console.log(this.stack.peek());
+
+    this.stack.clear();
+    console.log("length: " + this.stack.length());
+    console.log(this.stack.peek());
+
+    this.stack.push("Clayton");
+    console.log(this.stack.peek());
 
   }
 }
