80772583492a8c45feaa040127f46b84d475c6cf
SmaLLAlien <vlad_belousov1990@hotmail.com>
2022-11-27 14:22:09 +0200

    hashes: added basic hash

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -1,21 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, Validators} from "@angular/forms";
+import {HashTableService} from "../hash-table.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'data_structures';
   taskControl = new FormControl('', Validators.required);
   array = [];
 
-  addItemToList(): void {
-
+  constructor(public hash: HashTableService) {
   }
 
-  deleteItem(i: number): void {
+  addItemToList(): void {
+    const value = this.taskControl.value;
+    if (value) {
+      this.hash.put(value);
+    }
+  }
 
+  deleteItem(value: string): void {
+    this.hash.delete(value);
+  }
+
+  ngOnInit(): void {
+    const someNames = ["David", "Jennifer", "Donnie", "Raymond",
+      "Cynthia", "Mike", "Clayton", "Danny", "Jonathan"];
+
+    for (let i = 0; i < someNames.length; ++i) {
+      this.hash.put(someNames[i]);
+    }
+    this.hash.showDistro();
   }
 }
